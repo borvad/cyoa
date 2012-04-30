68c997511882033d8f1bddae7835f4ceafd3d6e5
Ben Orvad <ben.orvad@gmail.com>
2012-04-30 18:09:19 +0930

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChooseYourOwnAdventure::Application.config.secret_token = '68349bd025d1cc41c2c7d50f3f09f2e054eb5a46bbfeeab365ff7fd8480464d186b2e205f4f01ecaeacd8e3f0ba77f3eaffe150201e460111baea817f8090880'
